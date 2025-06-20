09f4e01096fa07b95938b8e720c05b0f4f8446a8
Weera-singhe <kavisandyy@gmail.com>
2025-06-20 23:35:47 +0530

    fixed not loading..

--- client/src/api/urls.jsx
+++ client/src/api/urls.jsx
@@ -1,4 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://mywebsite-backend-u5tr.onrender.com/api";
 
 export const ITEMS_API_URL = `${API_BASE_URL}/items`;
